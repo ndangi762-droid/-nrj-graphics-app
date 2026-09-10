883b6f68c2b6955910896dc08b8fd9488b385039
ndangi762-droid <ndangi762@gmail.com>
2026-09-10 09:18:19 +0530

    Add PRINTUP mobile app service worker

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = 'printup-shell-v1';
+const APP_SHELL = ['/', '/login', '/signup', '/manifest.json', '/nrj_graphics_icon.svg?v=2'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    if (response.ok && ['document', 'script', 'style', 'image', 'manifest'].includes(event.request.destination)) {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('/login'))));
+});
